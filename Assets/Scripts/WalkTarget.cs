418a8d4e50c2debacfe2d560080d83d4ea292307
Joanna Chin <jochin@Joannas-MacBook-Air.local>
2016-08-15 10:14:57 -0400

    updated WalkTarget script

--- Assets/Scripts/WalkTarget.cs
+++ Assets/Scripts/WalkTarget.cs
@@ -1,10 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-// Ugly script, just used for demo/testing purposes!
 public class WalkTarget : MonoBehaviour
 {
-	// Set this to your player transform (or any transform you want your Zombie to turn towards).
+	// Set this to your player transform (or target point)
 	public Transform Player;
 
 	// You can uncheck this in the inspector, to see how things fall apart without the awesomeness of MatchTarget.
@@ -16,27 +15,11 @@ public class WalkTarget : MonoBehaviour
 	{
 		Animator animator = GetComponent<Animator>();
 
-		// This part just waits for the walk animation to loop two full times (4 steps), then triggers a turn.
-		//  In my real project I'll trigger the turns using a series of way points, not a fixed number of steps.
-//		if (State == 0)
-//		{
-//			if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 2)
-//			{
-//				State = 1;
-////				animator.SetTrigger("turn");
-//			}
-//		}
-
-		// We are turning...
-//		else if (State == 1)
-//		{
-			// Wait for our turn state to begin (there is a delay because I'm using blending to make things look nice)...
+			// Wait for our turn state to begin
 			if (animator.GetCurrentAnimatorStateInfo(0).IsName("turn"))
 			{
-				// Change to next state
-//				State = 2;
 
-				// The magic happens here, unless disabled...
+				// boolean if statement to disable match target
 				if (UseTargetMatch)
 				{
 					// Calculate the correct rotation needed for us to face the player/target
@@ -59,34 +42,23 @@ public class WalkTarget : MonoBehaviour
 						// we start adjustments at the current time, "right now"
 						animator.GetCurrentAnimatorStateInfo(0).normalizedTime,
 
-						// for my animation, this is when both feet hit the ground, so at that point it shouldn't continue to rotate
-						//  so I want the rotation adjustments to complete at this point in the animation (50% point).
-						0.5f);
+						//want the rotation adjustments to complete at this point in the animation (50% point).
+						0.75f);
 				}
 			}
-//		}
 
-		// Waiting for turn to complete...
-//		else if (State == 2)
-//		{
-			// Wait for walk state to begin...
+			 //Wait for walk state to begin...
 			if (animator.GetCurrentAnimatorStateInfo(0).IsName("start_walking"))
 			{
-				// Change to next state -- for this test, we are done.
-//				State = 3;
 
-				// Pretty much the same thing we did for our turn.  Due to blending between our turn state and walk state, the first walk loop needs
-				//  to be adjusted also, otherwise we'll no longer be facing the target.
+				// Due to blending between turn state and walk state, the first walk loop needs to be adjusted also, otherwise we'll no longer be facing the target.
 				if (UseTargetMatch)
 				{
 					Quaternion correctRot = Quaternion.LookRotation(Player.position - transform.position);
 
-					// For this one we use 1 as the end point.  At the very end of the first walk loop, we want to be facing the target.
-					//  This works correctly because our walk cycle starts and ends at the same rotation point (none, or whichever direction we were facing when it
-					//   started, which is typical of a good walk animation designed for mecanim).
+					// For this use 1 as the end point so that NPC is facing player at the end of first walk animation loop.
 					animator.MatchTarget(animator.targetPosition, correctRot, AvatarTarget.Root, new MatchTargetWeightMask(new Vector3(0, 0, 0), 1), animator.GetCurrentAnimatorStateInfo(0).normalizedTime, 1);
 				}
 			}
 		}
-//	}
 }
